4e58e5f8a553723a3729247fe8538c3599eced13
Dhruvil Nakrani <dhnakrani80@gmail.com>
2024-09-21 10:46:42 +0530

    Create Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,7 +19,8 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Project />} />
+        <Route path="/projects" element={<Project />} />
       </Routes>
     </BrowserRouter>
-  );  
+  );
 }
