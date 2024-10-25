5cd6339d8e80ebf6f94e546882b89c0245a6135f
Dhruvil Nakrani <dhnakrani80@gmail.com>
2024-10-25 10:44:13 +0530

    Add GoogleAuth Functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profilePicture: {
+      type: String,
+      default:'https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png'
+    },
   },
   { timestamps: true }
 );
