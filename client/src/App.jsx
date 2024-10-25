7cedf4ed16e389faab65c6b7280ff6218056858d
Dhruvil Nakrani <dhnakrani80@gmail.com>
2024-10-25 15:01:03 +0530

    Update Header Component with user Data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Signin from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
@@ -17,14 +16,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Project />} />
         <Route path="/projects" element={<Project />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
-  
