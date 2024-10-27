de26d81202cc3dc96ccd669a7e7bbd1c009f0ab7
Dhruvil Nakrani <dhnakrani80@gmail.com>
2024-10-27 10:57:22 +0530

    Add Show More functionality to the posts results of the Dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -3,9 +3,11 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { Table } from "flowbite-react";
 import { Link } from "react-router-dom";
+
 export default function DashPost() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,19 +25,36 @@ export default function DashPost() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        } 
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
             <Table.Head>
-              <Table.HeadCell>Date Updated</Table.HeadCell>
-              <Table.HeadCell>Post Image</Table.HeadCell>
-              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
               <Table.HeadCell>
-                <span className="hidden md:block">Edit</span>
+                <span>Edit</span>
               </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
@@ -79,9 +98,17 @@ export default function DashPost() {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
-        <p>You have no Posts yet!</p>
+        <p>You have no posts yet!</p>
       )}
     </div>
   );
